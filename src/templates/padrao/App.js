8684b825ba9b16fb3a92466e8b67d6a6aa8ab8d4
morais948 <progmatheusmorais@gmail.com>
2021-12-07 22:11:17 -0300

    first :rocket:

---
+++ src/templates/padrao/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/sass/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>inicio de tudo</h1>
+    </div>
+  );
+}
+
+export default App;
